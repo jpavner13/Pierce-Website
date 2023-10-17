7db589e4299fff5771c166dd69ff1010266bdfae
jpavner13 <jpavner@gmail.com>
2023-10-17 09:12:21 -0600

    Changed About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,18 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styled from 'styled-components';
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styled from "styled-components";
 
-import React, {useEffect} from "react";
-import Aos from 'aos';
+import React, { useEffect } from "react";
+import Aos from "aos";
 import "aos/dist/aos.css";
 
-import Pierce from '../Images/Pierce.JPG'
-import Resume from '../Images/Resume.pdf'
-import Pro from '../Images/Pro.png'
-import Am from '../Images/ADD.png'
-import As from '../Images/AS.png'
+import Pierce from "../Images/Pierce.JPG";
+import Resume from "../Images/Resume.pdf";
+import Pro from "../Images/Pro.png";
+import Am from "../Images/ADD.png";
+import As from "../Images/AS.png";
 
-import '../components/CSS Files/wrapImage.css'
+import "../components/CSS Files/wrapImage.css";
 
 const StyledRow = styled(Row)`
   padding: 1rem;
@@ -27,7 +27,7 @@ const Styledh5 = styled.h5`
 `;
 
 const StyledText = styled.h1`
-  padding:3rem;
+  padding: 3rem;
   color: grey;
   font-size: 35px;
   text-align: center;
@@ -64,68 +64,68 @@ const StyledImg2 = styled.img`
 
 function AboutPage() {
   useEffect(() => {
-    Aos.init({duration: 2000});
+    Aos.init({ duration: 2000 });
   }, []);
   return (
     <Container fluid>
       <StyledRow>
-        <Col data-aos="fade-right" >
-          <StyledImg id="tv" src={Pierce}/>
+        <Col data-aos="fade-right">
+          <StyledImg id="tv" src={Pierce} />
         </Col>
         <Col>
           <Styledh5 data-aos="fade-down">About</Styledh5>
           <StyledText data-aos="fade-left">
-            Second year honors Engineering student majoring in Computer Science at the University of Colorado Boulder. I've been obsessed with mechanical and 
-            electrical engineering projects ever since middle school. Now Computer Science is my passion. I've obtained multiple professional certifications, and 
-            won numerous technology and engineering competitions. I work fast and diligently, and I want to use my skills and ambition to make a positive impact on 
-            the world.
+            I am a third year honors Engineering student majoring in Computer
+            Science at the University of Colorado Boulder. I've been obsessed
+            with mechanical and electrical engineering projects ever since
+            middle school. Now Computer Science is my passion. I've obtained
+            multiple professional certifications, gained expirience from
+            internships, and won numerous technology and engineering
+            competitions. I work fast and diligently, and I want to use my
+            skills and ambition to make a positive impact on the world.
           </StyledText>
         </Col>
       </StyledRow>
       <StyledRow data-aos="fade-up">
-        <StyledText1>
-          Skills:
-        </StyledText1>
+        <StyledText1>Skills:</StyledText1>
       </StyledRow>
       <StyledRow data-aos="fade-up">
-        <StyledText1>
-          Languages: 
-        </StyledText1>
+        <StyledText1>Languages:</StyledText1>
         <StyledText2>
-          c++, c#, Java, Python, HTML, CSS, Javascript, Ruby, React JS, Verilog, Arduino, and command line control.
+          c++, Python, Java, c#, c, Javascript, React JS, CSS, HTML, SQL, Ruby,
+          Scala
         </StyledText2>
       </StyledRow>
       <StyledRow data-aos="fade-up">
-        <StyledText1>
-          Software:
-        </StyledText1>
+        <StyledText1>Software:</StyledText1>
         <StyledText2>
-          Solidworks, Adobe Illustrator, Altium Designer, Eagle, Unity, Cura, Meshmixer, Excel, Google applications, Github, etc.
+          SOLIDWORKS, Adobe Illustrator, Adobe Photoshop, Docker, Firebase,
+          GitHub, GitLab, AWS, Command Line Control, Linux development, Zephyr
+          RTOS, CubeMX, ESP IDF, Free RTOS, MeshMixer, Cura, MarkForged, Excel,
+          Google Applications
         </StyledText2>
       </StyledRow>
       <StyledRow data-aos="fade-up">
         <Col>
-          <StyledText1>
-            Certifications:
-          </StyledText1>
+          <StyledText1>Certifications:</StyledText1>
         </Col>
         <Col>
-          <StyledImg2 src={Pro}/>
+          <StyledImg2 src={Pro} />
         </Col>
         <Col>
-          <StyledImg2 src={Am}/>
+          <StyledImg2 src={Am} />
         </Col>
         <Col>
-          <StyledImg2 src={As}/>
+          <StyledImg2 src={As} />
         </Col>
       </StyledRow>
       <StyledRow data-aos="fade-up">
         <StyledText2>
-          Quick learner, Creative, Strong interpersonal skills, Problem solver.
+          Quick learner, Creative, Strong interpersonal skills, Problem solver
         </StyledText2>
       </StyledRow>
       <StyledRow data-aos="fade-up">
-        <iframe src={Resume} title="Resume" width="600" height="800"/>
+        <iframe src={Resume} title="Resume" width="600" height="800" />
       </StyledRow>
     </Container>
   );
